ea339c3fc4f8bac461967fa9204d3caa564f61b8
mkiw1464-debug <mkiw1464@gmail.com>
2026-04-19 19:59:04 +0800

    Create get_bind.js

---
+++ netlify/functions/get_bind.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+    const token = event.queryStringParameters.token;
+
+    if (!token) {
+        return { statusCode: 400, body: JSON.stringify({ error: "Token diperlukan" }) };
+    }
+
+    try {
+        const response = await axios.get("https://100067.connect.garena.com/game/account_security/bind:get_bind_info", {
+            params: { app_id: "100067", access_token: token },
+            headers: {
+                'User-Agent': "GarenaMSDK/4.0.19P9(Redmi Note 5 ;Android 9;en;US;)",
+            }
+        });
+
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(response.data)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message })
+        };
+    }
+};
